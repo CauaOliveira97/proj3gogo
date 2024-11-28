b45573f2fdfbdca54231b43a75cacb355567f51d
CauaOliveira97 <168209332+CauaOliveira97@users.noreply.github.com>
2024-11-28 14:45:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'O que levou Getúlio Vargas ao poder?',
+    'Foi resultado direto da Revolução de 1930, que destituiu Washington Luís e impediu a posse de Júlio Prestes'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Angola?',
+    'A capital da Angola é Luanda'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é um dodô?',
+    'É uma espécie extinta de ave da família dos pombos que era endêmica de Maurício, uma ilha no Oceano Índico a leste de Madagascar'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual o verbo to be?',
+    'Am, Is, Are'
+)
